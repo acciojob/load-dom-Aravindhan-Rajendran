66cf9067bdfaad844c1f7da61adce15a6c5f0999
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 21:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 function myfunction() {
 	console.log("DOM load success");
+	document.body.textContent="DOM load success";
 }
 document.addEventListener("DOMContentLoaded", myfunction);
